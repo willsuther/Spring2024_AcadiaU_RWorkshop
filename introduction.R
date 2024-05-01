ce07a2171c4e86fff5abf06a6dacf56b99461b3e
Will Sutherland <willsutherland@acadiau.ca>
2024-05-01 15:10:20 -0300

    Added sections "Running Code" and "Mathematical Operators"

---
+++ introduction.R
@@ -0,0 +1,29 @@
+# ---- Running Code ----
+
+# if a line starts with #, R will not try to run any code
+# Any line starting with # is called a "comment"
+# Comments are important to help use organize and document our code
+
+# On windows, Ctrl+Enter to run a line of code
+# On Mac Cmd+Enter
+
+# You should see > in the lower left hand corner of the Console window
+# If you see + you have missed a piece of code and R is waiting
+# When we run code, the result will show in the console
+
+# If you want to reset a line in your console, click into your console and hit Esc
+# If you want to clear your console, Ctrl+L (you will lose your console history)
+
+
+# ---- Mathematical Operators ----
+
+# We can use R to act as a calculator to perform math
+# See the result in the console?
+2 + 2
+2 - 10
+5 * 5
+4/10
+(2+1)/13
+3^2
+
+# Now, clear your console!
